5902a0d6c2046b416392451d7f764213ba814c4b
KnightDark376 <82499844+KnightDark376@users.noreply.github.com>
2021-09-12 23:31:41 +0700

    Update FingerCountProject.py

--- FingerCountProject.py
+++ FingerCountProject.py
@@ -1,15 +1,15 @@
 import cv2
 import time
 import os
-import HandTrackingModule as htm
+import HandTrackingModule as htm #chèn module HandTrackingModule vào project
 
-id_camera = 0
-wCam, hCam = 400, 600
+id_camera = 0 #Địa chỉ camera kết nối
+wCam, hCam = 400, 600 # Chiều dài và chiều rộng khung ảnh
 
-cap = cv2.VideoCapture(id_camera)
-cap.set(3,wCam)
-cap.set(4, hCam)
-
+cap = cv2.VideoCapture(id_camera) #Đọc hình ảnh từ cameraid
+cap.set(3,wCam) #Cài đặt chiều rộng khung camera
+cap.set(4, hCam) #Cài đặt chiều dài khung camera
+'''Đọc file hình ảnh các hình chỉ thị số đếm'''
 folderPath = "FingerImages"
 myList = os.listdir(folderPath)
 print(myList)
@@ -18,22 +18,22 @@ for imPath in myList:
     image = cv2.imread(f'{folderPath}/{imPath}')
     #print(f'{folderPath}/{imPath}')
     overlaylist.append(image)
-
+'''------------------------------------------------'''
 print(len(overlaylist))
 pTime = 0
 
-detector = htm.handDetector(detectionCon=0.75)
+detector = htm.handDetector(detectionCon=0.75) # Khởi tạo
 
-tipIds = [4, 8, 12, 16, 20]
+tipIds = [4, 8, 12, 16, 20] #List các vị trí để xác định hoạt động của ngón tay
 
 while True:
     success, img = cap.read()
-    detector.findHands(img)
-    lmList = detector.findPosition(img, draw=False)
+    detector.findHands(img) #nhận diện bàn tay
+    lmList = detector.findPosition(img, draw=False) #Tìm các điểm mốc trên bàn tay
     #print(lmList)
 
     #cv2.rectangle(img, (20, 425), (425, 810), (0, 255, 0), cv2.FILLED)
-    if len(lmList) != 0:
+    if len(lmList) != 0: #Kiểm tra điều kiện có nhận được điểm mốc nào không
         fingers = []
 
         #Thumb
@@ -77,7 +77,7 @@ while True:
     else:
         cv2.putText(img, 'NoHand', (45, 375), cv2.FONT_ITALIC,
                     3, (255, 0, 0), 4)
-
+    '''Tính tốc độ khung hình'''
     cTime = time.time()
     fps = 1/(cTime-pTime)
     pTime = cTime
@@ -85,7 +85,7 @@ while True:
     cv2.putText(img, f'FPS: {int(fps)}', (400, 70), cv2.FONT_HERSHEY_PLAIN,
                 3,(255,0,0), 3)
 
-    cv2.imshow("image", img)
+    cv2.imshow("image", img) #hiển thị video lên màn hình
     if cv2.waitKey(1) & 0xFF = ord('q'):
         break
         
